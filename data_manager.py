859f10ab2cac90874ffb7d17ea0da6a4bc9564cb
Dalma Csernok <dlmcsernok@gmail.com>
2022-01-24 14:22:51 +0100

    basics structure is done

---
+++ data_manager.py
@@ -0,0 +1,15 @@
+import connection
+
+HEADER = ["id", "submission_time", "view_number", "vote_number", "title", "message", "image"]
+
+
+def add_question(question):
+    data = connection.read_file()
+    data.append(question)
+    connection.write_file()
+
+
+def post_answer():
+    pass
+
+def sort_question():
--- server.py
+++ server.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
+@app.route("/list")
+def list_questions():
     return "Hello World!"
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(
+        port=5000,
+        debug=True,
+    )
+
+@app.route("/question/<question_id>")
+@app.route("/add-question")
+@app.route("/question/<question_id>/new-answer")
+@app.route("/question/<question_id>/delete")
+@app.route("/question/<question_id>/edit")
+@app.route("/question/<question_id>/vote_up")
+@app.route("/question/<question_id>/vote_down")
+# Sorting: /list?order_by=title&order_direction=desc
+@app.route("/answer/<answer_id>/delete")
+@app.route("/answer/<answer_id>/vote_up")
+@app.route("/answer/<answer_id>/vote_down")
